68887f420476a2c892d3cde1d65645040da46b50
Grégory <gregory95am@gmail.com>
2024-11-16 22:09:36 +0100

    speed.py v2

--- Game/speed.py
+++ Game/speed.py
@@ -1,1 +1,17 @@
+# Speed.py - gestion de la vitesse.
+
+"""
+Vitesse du joueur.
+"""
 JUMP_HORIZONTAL_SPEED = 5
+
+"""
+Vitesse de la caméra
+"""
+# A venir
+
+"""
+Vitesse du gameplay
+"""
+
+# A voir / A venir
