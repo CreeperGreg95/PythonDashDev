b87aeeb2e3f1f221e14866db373b8fde3bc5d4a8
Grégory <gregory95am@gmail.com>
2024-11-16 22:48:15 +0100

    keybinds.py v2

--- Game/keybinds.py
+++ Game/keybinds.py
@@ -2,7 +2,7 @@ import pygame
 
 # keybinds.py
 
-def handle_keydown(event, player, options):
+def handle_event(event, player, options):
     """
     Gère les pressions sur les touches pour contrôler le joueur et les options.
     
@@ -10,18 +10,19 @@ def handle_keydown(event, player, options):
     :param player: L'objet Player pour exécuter les actions.
     :param options: Les options de jeu pour activer/désactiver des fonctionnalités.
     """
-    if event.key == pygame.K_SPACE:
-        # Le joueur saute verticalement
-        player.jump(0)
-        print("la touche espace a été pressée !")
-    elif event.key == pygame.K_UP:
-        # Le joueur saute verticalement
-        player.jump(0)
-        print("La touche flèche haute a été pressée !")
-    elif event.key == pygame.MOUSEBUTTONDOWN:
-        # Le joueur saute verticalement
-        player.jump(0)
-        print("Le clic gauche de la souris a été pressée !")
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            # Le joueur saute verticalement
+            player.jump(0)
+            print("la touche espace a été pressée !")
+        elif event.key == pygame.K_UP:
+            # Le joueur saute verticalement
+            player.jump(0)
+            print("La touche flèche haute a été pressée !")
+        elif event.key == pygame.K_h:
+            # Basculer la visibilité des hitboxes
+            options.toggle_hitboxes()
+
         """
         A mettre en fonctionnement quand le platformer sera developpé (pas tout de suite lol)
 
@@ -32,6 +33,8 @@ def handle_keydown(event, player, options):
             # Le joueur saute vers la droite
             player.jump(1)
         """
-    elif event.key == pygame.K_h:
-        # Basculer la visibilité des hitboxes
-        options.toggle_hitboxes()
+
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:  # Clic gauche
+            player.jump(0)
+            print("Le clic gauche de la souris a été pressée !")
