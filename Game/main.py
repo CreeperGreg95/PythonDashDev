5e798c8387e07242bde66d84989a22c648668fb2
Grégory <gregory95am@gmail.com>
2024-11-17 00:31:27 +0100

    Version 0.0.0.8

--- Game/main.py
+++ Game/main.py
@@ -1,4 +1,4 @@
-# main.py version 6
+# main.py version 7
 
 import pygame
 from player import Player
@@ -9,30 +9,20 @@ from keybinds import handle_event
 from grounds import Ground  # Importation de la classe Ground
 from backgrounds import Background  # Importation de la classe Background
 
+# Initialisation de Pygame et des options de jeu
+options = GameOptions()
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((options.screen_width, options.screen_height))
 pygame.display.set_caption("Python Dash")
 
 # Chargement des ressources et initialisation des objets
-options = GameOptions()
-
-# Calcul du décalage du sol (1/8 de la hauteur de la fenêtre)
-ground_height = 400 // 8  # 1/8 de la hauteur de l'écran
-
-# Crée le joueur avec la position ajustée
-player = Player(load_player_icon(), ground_height)
-
-# Crée l'obstacle en passant également ground_height
-obstacle = Obstacle(800, options.obstacle_speed, ground_height)
-
-# Crée le sol avec 5000 textures
-ground = Ground(800, 400, ground_height, texture_count=5000)
-
-# Crée le background avec 5000 images
-background = Background(800, 400, texture_count=5000)
+player = Player(load_player_icon(), options.screen_height // 8)  # Calcul du décalage du sol
+obstacle = Obstacle(options.screen_width, options.obstacle_speed, options.screen_height // 8)
+ground = Ground(options.screen_width, options.screen_height, options.screen_height // 8, texture_count=5000)
+background = Background(options.screen_width, options.screen_height, texture_count=5000)
 
 # Variable pour contrôler la vitesse du background (modifiable facilement)
-BACKGROUND_SPEED = options.obstacle_speed / 10  # Ici, le fond se déplace plus lentement que les obstacles
+BACKGROUND_SPEED = options.obstacle_speed * options.background_speed_factor  # Le fond se déplace plus lentement
 
 # Boucle de jeu
 running = True
@@ -74,7 +64,7 @@ while running:
 
     # Mise à jour de l'écran et limitation des FPS
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(options.fps_limit)  # Utilisation de la limite des FPS définie dans les options
 
 # Quitter Pygame
 pygame.quit()
--- Game/options.py
+++ Game/options.py
@@ -1,11 +1,42 @@
+# options.py version 2
+
 class GameOptions:
     def __init__(self):
-        self.obstacle_speed = 5
-        self.show_hitboxes = False  # Nouvelle option pour afficher/désactiver les hitboxes
+        # Paramètres de base du jeu
+        self.obstacle_speed = 5  # Vitesse des obstacles
+        self.show_hitboxes = False  # Option pour afficher/désactiver les hitboxes
+        
+        # Paramètres de la fenêtre
+        self.screen_width = 800  # Largeur de la fenêtre
+        self.screen_height = 400  # Hauteur de la fenêtre
+        
+        # Paramètres des FPS
+        self.fps_limit = 60  # Limite des FPS par défaut
+        
+        # Paramètres du background
+        self.background_speed_factor = 0.1  # Facteur de vitesse du background par rapport à la vitesse des obstacles
 
+    # Méthode pour augmenter la difficulté (augmentation de la vitesse des obstacles)
     def increase_difficulty(self):
         self.obstacle_speed += 0.5
 
+    # Méthode pour activer/désactiver l'affichage des hitboxes
     def toggle_hitboxes(self):
-        """Permet d'activer/désactiver l'affichage des hitboxes"""
         self.show_hitboxes = not self.show_hitboxes
+
+    # Méthode pour modifier les FPS du jeu
+    def set_fps_limit(self, fps):
+        """Permet de modifier la limite des FPS"""
+        self.fps_limit = fps
+
+    # Méthode pour changer la taille de la fenêtre
+    def set_window_size(self, width, height):
+        """Permet de modifier la taille de la fenêtre"""
+        self.screen_width = width
+        self.screen_height = height
+
+    # Méthode pour changer la vitesse du background
+    def set_background_speed_factor(self, factor):
+        """Permet de modifier la vitesse du background"""
+        self.background_speed_factor = factor
+
