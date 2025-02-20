881db21e74144e1dca33af8fab7619742b200f1c
Dilan103 <emmapasquier95@icloud.com>
2025-02-20 22:54:04 +0100

    SpeedEffect

--- Game/SpeedEffects.py
+++ Game/SpeedEffects.py
@@ -3,20 +3,18 @@ class SpeedEffect:
         """Applique l'effet du speed au joueur et ajuste la difficulté des obstacles."""
         if speed_type == "boost0.5":
             print("Le speed x0.5 a apparu.")
-            player.speed *= 0.95  # Diminution de la vitesse
+            player.speed *= 0.5  # Diminution de la vitesse
         elif speed_type == "boost4":
             print("Le speed x4 a apparu.")
-            player.speed *= 1.5  # Augmentation modérée de la vitesse
-            self.increase_difficulty()  # Augmenter la difficulté des obstacles
+            player.speed *= 4.0  # Augmentation modérée de la vitesse
         elif speed_type == "speed1":
             print("Le speed x1 a apparu.")
             player.speed *= 1.0  # Petit boost de la vitesse
         elif speed_type == "speed2":
             print("Le speed x2 a apparu.")
-            player.speed *= 1.2  # Boost moyen de la vitesse
+            player.speed *= 2.0  # Boost moyen de la vitesse
         elif speed_type == "speed3":
             print("Le speed x3 a apparu.")
-            player.speed *= 1.35  # Grand boost de la vitesse
-            self.increase_difficulty()  # Augmenter la difficulté des obstacles
+            player.speed *= 3.0  # Grand boost de la vitesse
 
         return player.speed
